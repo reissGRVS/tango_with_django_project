b5d08e876e9fd7f89b69020523ecb97df5c52962
Reiss <reissGRVS@gmail.com>
2018-01-29 10:22:48 +0000

    Chapter 10 Completed

--- rango/views.py
+++ rango/views.py
@@ -5,15 +5,44 @@ from django.contrib.auth import authenticate, login, logout
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
 	categories = Category.objects.order_by('-likes')[:5]
 	pages = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': categories, 'pages': pages}
-	return render(request, 'rango/index.html', context=context_dict)
+	
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
+	response = render(request, 'rango/index.html', context=context_dict)
+	
+	return response
+
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		#TODO: This makes no sense
+		visits = 1
+		request.session['last_visit'] = last_visit_cookie
+
+	request.session['visits'] = visits
 
 def about(request):
-	return render(request, 'rango/about.html', context={})
+	visitor_cookie_handler(request)
+	return render(request, 'rango/about.html', context={'visits':request.session['visits']})
 
 def show_category(request, category_name_slug):
 	context_dict = {}
